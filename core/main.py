49f253bb3a594e4bbbabfc4e1ac70f6d33d4147c
ded2322 <151318767+ded2322@users.noreply.github.com>
2024-04-05 20:27:09 +0700

    host frontend

--- core/main.py
+++ core/main.py
@@ -16,25 +16,22 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(chat_router)
-origins = [
-    # 3000 - порт, на котором работает фронтенд на React.js
-    "http://localhost:3000",
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
-                   "Access-Control-Allow-Origin", "Authorization"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+    expose_headers=['Access-Control-Allow-Origin']
 )
 
 
 app = VersionedFastAPI(app,
     version_format='{major}',
     prefix_format='/v{major}',
-    description="Это основная версия, находящиеся здесь ручки сделаны или почти сделаны ",
+    description="Это основная версия, находящиеся здесь ручки сделаны или почти сделаны",
     #middleware=[
     #    Middleware(SessionMiddleware, secret_key='mysecretkey')
     #]
